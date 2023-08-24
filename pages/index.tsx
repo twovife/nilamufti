86496554517dda51540e22076e8eba55e0ef4227
twovife <azizihsan666@gmail.com>
2023-08-24 10:35:29 +0700

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,10 +36,14 @@ const Index: NextPage = (props: any) => {
           name="description"
           content="Selamat datang di undangan pernikahan kami! Bergabunglah dengan kami dalam momen istimewa ini saat kami merayakan cinta dan kebahagiaan. Tanggal, waktu, dan tempat telah kami persiapkan dengan penuh kasih untuk Anda."
         />
-        <meta property="og:type" content="website" />
+        <meta property="og:type" content="website" />{" "}
+        <meta
+          property="og:description"
+          content="Selamat datang di undangan pernikahan kami! Bergabunglah dengan kami dalam momen istimewa ini saat kami merayakan cinta dan kebahagiaan. Tanggal, waktu, dan tempat telah kami persiapkan dengan penuh kasih untuk Anda."
+        />
         <meta property="og:locale" content="id_ID" />
         <meta property="og:site_name" content="Undangan Pernikahan" />
-        <meta property="og:image" content="/main.jpg" />
+        <meta property="og:image" content="/main2.jpg" />
         <meta property="og:image:width" content="500" />
         <meta property="og:image:height" content="500" />
       </Head>
