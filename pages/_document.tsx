e9e21fe719e51469d3f487c8ee149498c35b20cc
twovife <azizihsan666@gmail.com>
2023-08-24 10:53:08 +0700

    asd
    asd

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,14 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
-
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
         <Head>
           <title>Undangan Pernikahan Nila ❤️ Mufti</title>
+          <meta
+            property="og:title"
+            content="Undangan Pernikahan Nila ❤️ Mufti"
+          ></meta>
           <meta
             name="description"
             content="Selamat datang di undangan pernikahan kami! Bergabunglah dengan kami dalam momen istimewa ini saat kami merayakan cinta dan kebahagiaan. Tanggal, waktu, dan tempat telah kami persiapkan dengan penuh kasih untuk Anda."
@@ -15,9 +18,19 @@ export default class Document extends NextDocument {
             property="og:description"
             content="Selamat datang di undangan pernikahan kami! Bergabunglah dengan kami dalam momen istimewa ini saat kami merayakan cinta dan kebahagiaan. Tanggal, waktu, dan tempat telah kami persiapkan dengan penuh kasih untuk Anda."
           />
+          <meta
+            property="twitter:description"
+            content="Selamat datang di undangan pernikahan kami! Bergabunglah dengan kami dalam momen istimewa ini saat kami merayakan cinta dan kebahagiaan. Tanggal, waktu, dan tempat telah kami persiapkan dengan penuh kasih untuk Anda."
+          ></meta>
           <meta property="og:locale" content="id_ID" />
           <meta property="og:site_name" content="Undangan Pernikahan" />
-          <meta property="og:image" content="/main2.jpg" />
+          <meta property="og:image" content="../public/main2.jpg" />
+          <meta property="twitter:image" content="../public/main2.jpg"></meta>
+          <meta property="twitter:card" content="summary_large_image"></meta>
+          <meta
+            property="twitter:title"
+            content="Undangan Pernikahan Nila ❤️ Mufti"
+          ></meta>
           <meta property="og:image:width" content="500" />
           <meta property="og:image:height" content="500" />
         </Head>
