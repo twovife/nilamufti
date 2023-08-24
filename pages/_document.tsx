7f6ec8a5cc72185e1bd04bef04a44a4f72038e99
twovife <azizihsan666@gmail.com>
2023-08-24 10:45:55 +0700

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,23 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <title>Undangan Pernikahan Nila ❤️ Mufti</title>
+          <meta
+            name="description"
+            content="Selamat datang di undangan pernikahan kami! Bergabunglah dengan kami dalam momen istimewa ini saat kami merayakan cinta dan kebahagiaan. Tanggal, waktu, dan tempat telah kami persiapkan dengan penuh kasih untuk Anda."
+          />
+          <meta property="og:type" content="website" />{" "}
+          <meta
+            property="og:description"
+            content="Selamat datang di undangan pernikahan kami! Bergabunglah dengan kami dalam momen istimewa ini saat kami merayakan cinta dan kebahagiaan. Tanggal, waktu, dan tempat telah kami persiapkan dengan penuh kasih untuk Anda."
+          />
+          <meta property="og:locale" content="id_ID" />
+          <meta property="og:site_name" content="Undangan Pernikahan" />
+          <meta property="og:image" content="/main2.jpg" />
+          <meta property="og:image:width" content="500" />
+          <meta property="og:image:height" content="500" />
+        </Head>
         <body className="antialiased">
           <Main />
           <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -30,23 +30,7 @@ const Index: NextPage = (props: any) => {
 
   return (
     <>
-      <Head>
-        <title>Undangan Pernikahan Nila ❤️ Mufti</title>
-        <meta
-          name="description"
-          content="Selamat datang di undangan pernikahan kami! Bergabunglah dengan kami dalam momen istimewa ini saat kami merayakan cinta dan kebahagiaan. Tanggal, waktu, dan tempat telah kami persiapkan dengan penuh kasih untuk Anda."
-        />
-        <meta property="og:type" content="website" />{" "}
-        <meta
-          property="og:description"
-          content="Selamat datang di undangan pernikahan kami! Bergabunglah dengan kami dalam momen istimewa ini saat kami merayakan cinta dan kebahagiaan. Tanggal, waktu, dan tempat telah kami persiapkan dengan penuh kasih untuk Anda."
-        />
-        <meta property="og:locale" content="id_ID" />
-        <meta property="og:site_name" content="Undangan Pernikahan" />
-        <meta property="og:image" content="/main2.jpg" />
-        <meta property="og:image:width" content="500" />
-        <meta property="og:image:height" content="500" />
-      </Head>
+      {/* <Head></Head> */}
       <div className="relative">
         <AnimatePresence>
           {isCoverVisible && (
